979632aca18f33d827646b81e4080f9d396c883d
welleson-lukas <wellesonlukas@gmail.com>
2021-05-18 12:48:52 -0300

    first commit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Teste(models.Model):
+    nome = models.CharField(max_length=255)
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
---
+++ cron.py
@@ -0,0 +1,5 @@
+from app.models import *
+
+
+def my_schuduler_job():
+    Teste.objects.create(nome='Lukas')
